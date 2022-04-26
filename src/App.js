d5b2f974807c82349861cbfc8b78e8ba4a0b3bcb
denis <permwed@gmail.com>
2022-04-26 13:22:06 +0500

    create header

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
-import "./App.css";
+import "./App.scss";
 
 function App() {
-  return <div className="App">!!!</div>;
+  return <div className="App">
+    <div className="header">Star wars: Planets</div>
+    <div className="content">123</div>
+  </div>;
 }
 
 export default App;
